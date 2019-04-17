fb0cf2b8caa7c525643ae67437ea00b470875435
Brian Tehrani <brian.tehrani@gmail.com>
2019-04-16 17:32:39 -0700

    Added default and overloaded constructors

--- Project1-CS20A/Student.cpp
+++ Project1-CS20A/Student.cpp
@@ -1,4 +1,22 @@
 /*
 * Implementations for the student.h functions
 *
-/
+*/
+#include "Student.h"
+
+Student::Student(){ //setting members to arbitrary default values
+	id = 00000000;
+	name = "empty";
+	school = "n/a";
+}
+
+Student::Student(int id, string name, string school) {
+	
+	this->id = id;
+	this->name = name;
+	this->school = school;
+
+}
+
+
+
